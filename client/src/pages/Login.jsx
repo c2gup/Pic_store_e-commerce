a7c45529a9686411a0723d20d05bc69bbeb72777
Gupta <c2gupt@gmail.com>
2024-11-17 18:46:34 +0530

    Dashboard completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function SignInThree() {
 
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/v1/login`,
+        `${import.meta.env.VITE_API_URL}/api/login`,
         {
           // Change 'name' to 'username' to match the backend
           email,
