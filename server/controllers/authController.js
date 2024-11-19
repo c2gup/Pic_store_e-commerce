86faa009c586f37b9b4962248c1193e350029529
Gupta <c2gupt@gmail.com>
2024-11-19 23:28:52 +0530

    day17/add/protectedroute

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,93 +1,142 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const {generateAccessToken} = require("../helpers/accessToken");
+const { generateAccessToken } = require("../helpers/accessToken");
+const { generateRefreshToken } = require("../helpers/refreshToken");
 
-const signUp = async (req,res) =>{
+const signup = async (req, res) => {
+  const { username, email, password, accountType } = req.body;
 
-      const {username ,email, password ,accountType} = req.body;
-       try {
+  try {
+    let user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already in use" });
+    }
+    const securePassword = await bcrypt.hash(password, 10);
 
-            let user = await User.findOne({username});
-            if(user){
-                  return res
-                  .status(400)
-                  .json({success:false,message:"User already exists"});
-            }
+    user = new User({
+      username,
+      email,
+      password: securePassword,
+      accountType,
+    });
+    await user.save();
 
-            const securePassword = await bcrypt.hash(password,10);
-
-            user = new User({
-                  username,
-                  email,
-                  password:securePassword,
-                  accountType,
-            });
-
-            await user.save();
-
-            return res
-            .status(201)
-            .json({success:true, message:"User created successfully"})
-            
-       } catch (error) {
-
-            return res.status(500).json({success:false,message:"erroe.message"});
-            
-       }
+    return res
+      .status(201)
+      .json({ success: true, message: "User created successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
-const login = async (req,res) =>{
-      const {email,password} = req.body;
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Please signup" });
+    }
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentails" });
 
-      try {
+    const data = {
+      id: user._id,
+      accountType: user.accountType,
+      author: user.username,
+    };
 
-            let user = await User.findOne({email});
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
 
-            if(!user){
-                  return res
-                  .status(404)
-                  .json({success:false,message:"user not found"});
-            }
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      accessToken,
+      refreshToken,
+      role: user.accountType,
+      author: user.username,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-            let comparepassword = await bcrypt.compare(password,user.password);
+const refresh = async (req, res) => {
+  const authHeader = req.headers["authorization"];
 
-            if(!comparepassword){
-                  return res
-                  .status(404)
-                  .json({success:false,message:"password mismatch"});
-            }
+  const token = authHeader && authHeader.split(" ")[1];
 
-            const data = {
-                  id: user._id,
-                  accountType: user.accountType,
-                  author: user.username,
-                };
+  if (!token)
+    return res.status(401).json({ success: false, message: "Please login" });
 
+  try {
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+      if (err)
+        return res.status(403).json({ success: false, message: err.message });
 
-                const accessToken = generateAccessToken(data);
-            //     const refreshToken = generateRefreshToken(data);
+      const accessToken = generateAccessToken({
+        id: user.id,
+        accountType: user.accountType,
+        author: user.author,
+      });
+      const refreshToken = generateRefreshToken({
+        id: user.id,
+        accountType: user.accountType,
+        author: user.author,
+      });
 
-            return res
-            .status(200)
-            .json({
-                  success:true,
-                  message:"access token",
-                  accessToken,
-                  role: user.accountType,
-                  author: user.username,
-            });
-            
-      } catch (error) {
-            return res
-            .status(404)
-            .json({
-                  success:false,
-                  message: error.message,
-            })
-      }
-}
+      return res.status(200).json({
+        success: true,
+        message: "Token refreshed successfully",
+        accessToken,
+        refreshToken,
+        role: user.accountType,
+        author: user.author,
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
+const switchProfile = async (req, res) => {
+  const authorId = req.id;
+  const authorAccountType = req.accountType;
+  try {
+    const user = await User.findByIdAndUpdate(authorId, {
+      accountType: authorAccountType === "buyer" ? "seller" : "buyer",
+    });
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
 
+    const data = {
+      id: user._id,
+      accountType: user.accountType,
+      author: user.username,
+    };
 
-module.exports = {  signUp , login };
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    return res.status(200).json({
+      success: true,
+      message: `Switched to ${user.accountType}`,
+      accessToken,
+      refreshToken,
+      role: user.accountType,
+      author: user.username,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { login, signup, refresh, switchProfile };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const controllers = require("./routes/authRoute")
+// const controllers = require("./routes/authRoute")
 require('dotenv').config();
 app.use(express.json());
 const cors = require('cors');
