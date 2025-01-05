f73e19b9255b91decc0f404afa8c3198e4c9742b
Gupta <c2gupt@gmail.com>
2025-01-06 01:11:29 +0530

    anupam/v2/google

--- client/utils/firebase.js
+++ client/utils/firebase.js
@@ -1,14 +1,44 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+//   measurementId: import.meta.env.VITE_MES,
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+// const provider = new GoogleAuthProvider();
+
+// export default async function googleAuth() {
+//   try {
+//     let data = await signInWithPopup(auth, provider);
+//     return data.user;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+
+
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
-import { getAuth,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+// Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
@@ -19,22 +49,26 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MES,
 };
 
-
-
-
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and Google Provider
 const auth = getAuth(app);
-
 const provider = new GoogleAuthProvider();
- 
+provider.setCustomParameters({
+  prompt: "select_account", // Forces user to select an account every time
+});
+
+// Export the Google Authentication function
 export default async function googleAuth() {
   try {
-    let data = await signInWithPopup(auth, provider);
-    return data.user;
+    const data = await signInWithPopup(auth, provider);
+    return data.user; // Returns the authenticated user object
   } catch (err) {
-  console.log(err);
-
+    console.error("Error during Google authentication:", err.message);
+    throw err; // Optional: re-throw the error for further handling
   }
 }
 
+// Optionally export the app and auth for reuse
+export { app, auth, provider };
